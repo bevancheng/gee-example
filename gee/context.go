0bfd4c980f28af29a2cdeeb45002ea19d9001d75
chengpf <call15222758360@163.com>
2022-06-02 02:21:35 +0800

    finish middleware

--- gee/context.go
+++ gee/context.go
@@ -19,6 +19,9 @@ type Context struct {
 	Params map[string]string
 	//response info
 	StatusCode int
+	//middleware
+	handlers []HandlerFunc
+	index    int //记录当前执行到第几个中间件
 }
 
 func (c *Context) Param(key string) string {
@@ -33,6 +36,15 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 		Req:    req,
 		Path:   req.URL.Path,
 		Method: req.Method,
+		index:  -1,
+	}
+}
+
+func (c *Context) Next() {
+	c.index++
+	s := len(c.handlers)
+	for ; c.index < s; c.index++ {
+		c.handlers[c.index](c) //加入中间件模块后在这里执行
 	}
 }
 
--- gee/gee.go
+++ gee/gee.go
@@ -3,6 +3,7 @@ package gee
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 type HandlerFunc func(*Context)
@@ -52,11 +53,22 @@ func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
 	group.addRoute("POST", pattern, handler)
 }
 
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
 func (e *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, e)
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range e.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
 	c := newContext(w, req)
+	c.handlers = middlewares
 	e.router.handle(c)
 }
